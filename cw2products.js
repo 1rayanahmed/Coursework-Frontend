127360d770f5d9c563c6ad1208dc6d399bc87453
1rayanahmed <rayanasif2004@gmail.com>
2024-12-11 19:36:09 +0400

    Add files via upload

---
+++ cw2products.js
@@ -0,0 +1,112 @@
+let products = [
+    {
+       _id:"67428bd23ec9bea87f963e40",
+        id: 2001,
+        title: "Math",
+        description: "Enhance your math skills with fun and interactive lessons. Topics include algebra, geometry, and problem-solving techniques.",
+        price: 35,
+        location: "London",   
+        image: "public/math.jpeg",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e42",
+        id: 2002,
+        title: "Writing",
+        description: "Unlock your creativity through storytelling, poetry, and essay writing.",
+        price: 40,
+        location: "Liverpool",  
+        availableInventory: 15,
+        image: "public/writing.jpg",
+        rating: 1
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e43",
+        id: 2003,
+        title: "Science",
+        description: "Dive into exciting experiments and learn complex scientific concepts from biology, physics, and chemistry.",
+        price: 45,  
+        location: "Manchester", 
+        image: "public/science.jpg",
+        availableInventory: 12,
+        rating: 3
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e44",
+        id: 2004,
+        title: "History and Social Studies",
+        description: "Explore world history and social studies with engaging lessons and discussions on key historical events and civilizations.",
+        price: 55,
+        location: "London", 
+        image: "public/history.jpg",
+        availableInventory: 5,
+        rating: 2
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e45",
+        id: 2005,
+        title: "Computer Programming",
+        description: "Get started with coding by learning Python, JavaScript, and web development in an interactive and fun environment.",
+        price: 60, 
+        location: "London",
+        image: "public/cs.jpg",
+        availableInventory: 7,
+        rating: 4
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e46",
+        id: 2006,
+        title: "Art and Design",
+        description: "Unleash your creativity with lessons on painting, sketching, and graphic design.",
+        price: 50, 
+        location: "Manchester", 
+        image: "public/art.jpeg",
+        availableInventory: 5,
+        rating: 3
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e47",
+        id: 2007,
+        title: "Spanish Language",
+        description: "Start learning Spanish through conversation, reading, and writing exercises.",
+        price: 40,  
+        location: "Liverpool",
+        image: "public/spanish.jpg",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e48",
+        id: 2008,
+        title: "French Language",
+        description: "Start learning French through conversation, reading, and writing exercises.",
+        price: 45,  
+        location: "Liverpool",
+        image: "public/french.jpg",
+        availableInventory: 6,
+        rating: 2
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e49",
+        id: 2009,
+        title: "Debate",
+        description: "Build confidence and enhance your communication skills by learning debate strategies.",
+        price: 50, 
+        location: "Manchester", 
+        image: "public/debate.jpg",
+        availableInventory: 8,
+        rating: 2
+    },
+    {
+        _id:"67428c8c3ec9bea87f963e4a",
+        id: 2010,
+        title: "Robotics and Engineering",
+        description: "Engage with hands-on robotics projects and explore the world of engineering and mechanics.",
+        price: 65,
+        image: "public/robo.jpg",
+        location: "London", 
+        availableInventory: 8,
+        rating: 3
+    }
+]
